662109a4665f933d5596e61b7ade802b33be2216
l18732905600 <l18732905600@163.com>
2018-11-01 21:06:07 +0800

    010

---
+++ project/src/cn/itcast/com/Demo.java
@@ -0,0 +1,11 @@
+package cn.itcast.com;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Project");
+
+    for(int i=0;i<10;i++ ){
+        System.out.println("P9");
+    }
+    }
+}
---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.printf("Computer");
+
+//        Random ra =new Random();
+        for (int i =0;i<10;i++){
+            System.out.println("1");
+        }
+    }
+}
